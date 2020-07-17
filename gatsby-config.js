d550c63738b52181f1238d6f9c44fa76ac432ac6
Aniket Gargya <gargya.aniket@gmail.com>
2020-07-17 14:31:34 -0500

    trying to use pwa

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,19 @@
 module.exports = {
     plugins: [
         {
+            resolve: "gatsby-plugin-manifest",
+            options: {
+                name: "GatsbyJS",
+                short_name: "GatsbyJS",
+                start_url: "/",
+                background_color: "#f7f0eb",
+                theme_color: "#a2466c",
+                display: "standalone",
+                icon: "src/images/icon.png",
+                theme_color_in_head: false,
+            }
+        },
+        {
             resolve: "gatsby-plugin-offline",
             options: {
                 precachePages: ["/"],
